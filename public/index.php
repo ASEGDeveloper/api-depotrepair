6e8174f51a7c039621d68a36639d79486ee5a8e6
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-10 18:27:04 +0530

    testing

--- public/index.php
+++ public/index.php
@@ -1,20 +1,23 @@
 <?php
 
-$serverName = "192.168.5.139,1433";
+phpinfo();
 
-$connectionInfo = [
-    "Database" => "deporepair",
-    "UID" => "depouser",
-    "PWD" => "P@33w0rd",
-    "Encrypt" => "no",                  // disable encryption
-    "TrustServerCertificate" => "yes",  // avoid certificate validation
-    "LoginTimeout" => 30
-];
+exit; 
+// $serverName = "192.168.5.139,1433";
 
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+// $connectionInfo = [
+//     "Database" => "deporepair",
+//     "UID" => "depouser",
+//     "PWD" => "P@33w0rd",
+//     "Encrypt" => "no",                  // disable encryption
+//     "TrustServerCertificate" => "yes",  // avoid certificate validation
+//     "LoginTimeout" => 30
+// ];
 
-if ($conn) {
-    echo "Connected!";
-} else {
-    print_r(sqlsrv_errors());
-}
+// $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+// if ($conn) {
+//     echo "Connected!";
+// } else {
+//     print_r(sqlsrv_errors());
+// }
