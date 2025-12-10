550d4d517d1660dd5fdfb066318c074d4f514ddf
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-10 18:30:12 +0530

    pdo

--- public/index.php
+++ public/index.php
@@ -1,23 +1,23 @@
 <?php
 
-phpinfo();
+// phpinfo();
 
-exit; 
-// $serverName = "192.168.5.139,1433";
+// exit; 
+$serverName = "192.168.5.139,1433";
 
-// $connectionInfo = [
-//     "Database" => "deporepair",
-//     "UID" => "depouser",
-//     "PWD" => "P@33w0rd",
-//     "Encrypt" => "no",                  // disable encryption
-//     "TrustServerCertificate" => "yes",  // avoid certificate validation
-//     "LoginTimeout" => 30
-// ];
+$connectionInfo = [
+    "Database" => "deporepair",
+    "UID" => "depouser",
+    "PWD" => "P@33w0rd",
+    "Encrypt" => "no",                  // disable encryption
+    "TrustServerCertificate" => "yes",  // avoid certificate validation
+    "LoginTimeout" => 30
+];
 
-// $conn = sqlsrv_connect($serverName, $connectionInfo);
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-// if ($conn) {
-//     echo "Connected!";
-// } else {
-//     print_r(sqlsrv_errors());
-// }
+if ($conn) {
+    echo "Connected!";
+} else {
+    print_r(sqlsrv_errors());
+}
