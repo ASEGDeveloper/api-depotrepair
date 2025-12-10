975965a758f4817e8fecae75c69a36990ca93406
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-10 15:22:46 +0530

    ODBC testing

--- public/index.php
+++ public/index.php
@@ -1,32 +1,26 @@
 <?php
 
-echo phpinfo();
+//echo phpinfo();
 
-exit;
-// define("APP_DB_HOST", '192.168.5.139');
-// define("APP_DB_USER", 'depouser');
-// define("APP_DB_PASSWORD", 'P@33w0rd');
-// define("APP_DB_DATABASE", 'deporepair');
+ 
 
-// // SQLSRV connection config
-// $connectionInfo = [
-//     "Database" => APP_DB_DATABASE,
-//     "UID"      => APP_DB_USER,
-//     "PWD"      => APP_DB_PASSWORD,
-//     "CharacterSet" => "UTF-8",
-//     "Encrypt" => "No",  // Disable SSL for testing
-//     "TrustServerCertificate" => true
-// ];
+$server = "192.168.5.139";
+$connectionInfo = [
+    "Database" => "deporepair",
+    "UID" => "depouser",
+    "PWD" => "P@33w0rd",
+    "Encrypt" => "No",
+    "TrustServerCertificate" => true
+];
 
-// // Try to connect
-// $conn = sqlsrv_connect(APP_DB_HOST, $connectionInfo);
+$conn = sqlsrv_connect($server, $connectionInfo);
 
-// if ($conn) {
-//     echo "<h3>Database Connected Successfully ✔</h3>";
-// } else {
-//     echo "<h3>Database Connection Failed ❌</h3>";
-//     echo "<pre>";
-//     print_r(sqlsrv_errors());
-//     echo "</pre>";
-// }
+if ($conn) {
+    echo "✔ Connected Successfully";
+} else {
+    echo "❌ Connection Failed<br>";
+    print_r(sqlsrv_errors());
+}
+
+
 ?>
