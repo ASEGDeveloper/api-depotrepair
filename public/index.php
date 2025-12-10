a673965590814538fa38669aec9b726913ebfd1f
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-10 18:34:03 +0530

    odbc test

--- public/index.php
+++ public/index.php
@@ -4,20 +4,18 @@
 
 // exit; 
 $serverName = "192.168.5.139,1433";
-
 $connectionInfo = [
     "Database" => "deporepair",
     "UID" => "depouser",
     "PWD" => "P@33w0rd",
-    "Encrypt" => "no",                  // disable encryption
-    "TrustServerCertificate" => "yes",  // avoid certificate validation
-    "LoginTimeout" => 30
+    "Encrypt" => true,                 // enable encryption
+    "TrustServerCertificate" => true,  // bypass certificate check
 ];
 
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if ($conn) {
-    echo "Connected!";
+if (!$conn) {
+    die(print_r(sqlsrv_errors(), true));
 } else {
-    print_r(sqlsrv_errors());
+    echo "Connected successfully!";
 }
