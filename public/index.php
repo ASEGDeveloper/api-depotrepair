9c62499114d73eb165f8f2f49090657b3637e551
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-10 17:22:49 +0530

    odbc

--- public/index.php
+++ public/index.php
@@ -1,32 +1,20 @@
 <?php
 
-// 192.168.5.139,1433
-$serverName = "192.168.5.139"; // SQL Server IP and port
+$serverName = "192.168.5.139,1433";
 
 $connectionInfo = [
     "Database" => "deporepair",
     "UID" => "depouser",
     "PWD" => "P@33w0rd",
-    // Disable encryption for SQL Server 2014 compatibility
-    "Encrypt" => false,
-    "TrustServerCertificate" => true,
+    "Encrypt" => "no",                  // disable encryption
+    "TrustServerCertificate" => "yes",  // avoid certificate validation
+    "LoginTimeout" => 30
 ];
 
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 if ($conn) {
-    echo "✔ Connected Successfully!";
+    echo "Connected!";
 } else {
-    echo "❌ Connection Failed!<br>";
-    if (($errors = sqlsrv_errors()) != null) {
-        foreach ($errors as $error) {
-            echo "SQLSTATE: " . $error['SQLSTATE'] . "<br>";
-            echo "Code: " . $error['code'] . "<br>";
-            echo "Message: " . $error['message'] . "<br>";
-        }
-    }
+    print_r(sqlsrv_errors());
 }
-
-// Optional: Close connection
-// sqlsrv_close($conn);
-?>
