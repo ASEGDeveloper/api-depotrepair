f0fbb0cd245f49d5ea581328255307edcb4dced0
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-10 15:33:08 +0530

    port

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
 
 //echo phpinfo();
-$serverName = "192.168.5.139";
+//$serverName = "192.168.5.139";
+$serverName = "192.168.5.139,1433"; 
 
 $connectionInfo = [
     "Database" => "deporepair",
     "UID" => "depouser",
     "PWD" => "P@33w0rd",
-    "Encrypt" => 0,
+    "Encrypt" => "no",
     "TrustServerCertificate" => 1
 ];
 
