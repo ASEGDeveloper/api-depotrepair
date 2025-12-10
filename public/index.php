b50012ddfc1a6d4e2e8378f81faf906137cca1cc
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-10 18:20:52 +0530

    odbc

--- public/index.php
+++ public/index.php
@@ -1,20 +1,20 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+$serverName = "192.168.5.139,1433";
 
-define('LARAVEL_START', microtime(true));
+$connectionInfo = [
+    "Database" => "deporepair",
+    "UID" => "depouser",
+    "PWD" => "P@33w0rd",
+    "Encrypt" => "no",                  // disable encryption
+    "TrustServerCertificate" => "yes",  // avoid certificate validation
+    "LoginTimeout" => 30
+];
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if ($conn) {
+    echo "Connected!";
+} else {
+    print_r(sqlsrv_errors());
 }
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
