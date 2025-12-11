35f868c27f2658a3fd953bdfcaa728c067a30316
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-11 16:08:50 +0530

    test

--- public/index.php
+++ public/index.php
@@ -11,22 +11,22 @@
 //     "TrustServerCertificate" => true
 // ];
 
+$serverName = "192.168.5.139,1433";
 
+$connectionInfo = [
+    "Database" => "DEPOREPAIR",
+    "UID" => "pdms",
+    "PWD" => "PhpC@ase2608",
+    "Encrypt" => false,
+    "TrustServerCertificate" => true
+];
 
-// $serverName = "192.168.5.139"; // No need to include port if default 1433
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-// $connectionInfo = [
-//     "Database" => "DEPOREPAIR",
-//     "UID" => "pdms",
-//     "PWD" => "PhpC@ase2608",
-//     "Encrypt" => "Optional",       // Use Optional for ODBC 18
-//     "TrustServerCertificate" => true
-// ];
-try {
-    $dsn = "sqlsrv:Server=192.168.5.139,1433;Database=DEPOREPAIR;Encrypt=no;TrustServerCertificate=yes";
-    $conn = new PDO($dsn, "pdms", "PhpC@ase2608");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully with PDO!";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+if (!$conn) {
+    echo "Connection failed:\n";
+    die(print_r(sqlsrv_errors(), true));
+} else {
+    echo "Connected successfully!";
+    sqlsrv_close($conn);
 }
