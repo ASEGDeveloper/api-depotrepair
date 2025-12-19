b9104b227f55ff319d582082f119522e0fb73d88
Haridwar Yadav <haridwar.yadav@servoedge.com>
2025-12-19 17:51:27 +0530

    personal access token

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -6,6 +6,6 @@ use Laravel\Sanctum\PersonalAccessToken as SanctumToken;
 class PersonalAccessToken extends SanctumToken
 {
     // Explicitly specify the schema-qualified table name
-   // protected $table = 'deporepair.personal_access_tokens';
-   protected $table = 'personal_access_tokens';
+    protected $table = 'deporepair.personal_access_tokens';
+  // protected $table = 'personal_access_tokens';
 }
